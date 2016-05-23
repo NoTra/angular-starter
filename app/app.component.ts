8fe8a76880b84ef6e1e8802898265f9aac4264ce
Bruno TRAN <bruno.tran@ladepeche.fr>
2016-05-23 17:26:37 +0200

    - Création du component hero-detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,40 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{ title }}</h1>
-    <h2>{{ hero }} details !</h2>
-    <p>Heroes : </p>
-    <ul>
-      <li *ngFor="let hero of heroes">{{ hero }}</li>
+    <h3>Heroes</h3>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === currentHero">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
     </ul>
-  `
+    <my-hero-detail [hero]="currentHero"></my-hero-detail>
+  `,
+  directives: [HeroDetailComponent]
 })
 
 export class AppComponent {
-  title: string;
-  hero: string;
-  heroes: string[];
+  public title: string;
+  public currentHero: Hero;
+  public heroes: Hero[];
 
   constructor() {
     this.title = 'Tour of Heroes';
-    this.heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-    this.hero = this.heroes[0];
+    this.heroes = [
+      new Hero(1, 'Windstorm'),
+      new Hero(2, 'Bombasto'),
+      new Hero(3, 'Magneta'),
+      new Hero(4, 'Tornado'),
+      new Hero(5, 'Mr. Nice'),
+      new Hero(6, 'Narco'),
+      new Hero(7, 'Dr IQ')
+    ];
+    // this.currentHero = this.heroes[0];
   }
+
+  onSelect(hero: Hero) { this.currentHero = hero; }
 }
---
+++ app/hero.ts
@@ -0,0 +1,9 @@
+export class Hero {
+  public id: number;
+  public name: string;
+
+  constructor (i, n) {
+    this.id = i;
+    this.name = n;
+  }
+}
